a6e1997379edda00979e4fdc0cb751d0ec938caa
BoroaDev7 <jmborjas@unitec.edu>
2023-06-14 23:18:31 -0600

    "Conexion Mongo"

--- express/server.js
+++ express/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+
+const usuarioSchema = require("./usuarios");
+const usuarios = mongoose.model("usuario",usuarioSchema); 
+const MongoUri = "mongodb+srv://GrupoUX:ProyectoUX2023@cluster0.4nq8gyr.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(MongoUri, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log("Conectado a mongo")
+})
+
 
 app.get('/', (req, res) => {
   res.send('Conexion establecida');
+});
+
+app.get('/usuarios', (req, res) => {
+  Usuario.find({}, (error, usuarios) => {
+    if (error) {
+      console.log('Error al obtener usuarios:', error);
+      res.status(500).json({ error: 'Error al obtener usuarios' });
+    } else {
+      res.json(usuarios);
+    }
+  });
 });
 
 app.listen(port, () => {
