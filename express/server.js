0b73a4e9d0d7c0f28ca8a5d20611b1b1160fbea2
BoroaDev7 <jmborjas@unitec.edu>
2023-06-15 22:08:26 -0600

    Avance esquemas de nuevas tablas

--- express/server.js
+++ express/server.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 
 const Usuario = require("./schemas/usuarios");
 const Favorito = require("./schemas/favoritos");
+const Propiedad = require("./schemas/propiedades");
+const Detalles_Propiedad = require("./schemas/detalles_propiedad");
+const detalles_propiedad = require('./schemas/detalles_propiedad');
 
 const MongoUri = "mongodb+srv://GrupoUX:ProyectoUX2023@cluster0.4nq8gyr.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(MongoUri, { 
@@ -34,6 +37,87 @@ app.get('/usuarios', (req, res) => {
     }
   });
 });
+app.post('/agregarUsuario', (req, res) => {
+  const nuevoUsuario = new Usuario({
+      id: req.body.id,
+      nombre: req.body.nombre,
+      contrasenia: req.body.contrasenia,
+      correo: req.body.correo,
+  });
+  nuevoUsuario.save()
+  .then(usuario => {
+      res.json(usuario);
+  })
+  .catch(error => {
+      res.status(500).send(error);
+  });
+});
+
+
+
+
+app.get('/propiedades', (req, res) => {
+  Propiedad.find({}, (error, propiedades) => {
+    if (error) {
+      console.log('Error al obtener propiedades:', error);
+      res.status(500).json({ error: 'Error al obtener propiedades' });
+    } else {
+      res.json(propiedades);
+    }
+  });
+});
+
+app.post('/agregarPropiedades', (req, res) => {
+  const nuevoPropiedad= new Propiedad({
+      id_propiedad: req.body.id_propiedad,
+      id_usuario: req.body.id_usuario,
+      localizamiento: req.body.localizamiento,
+      precio: req.body.precio,
+      cuartos: req.body.cuartos,
+      banios: req.body.banios,
+  });
+  nuevoPropiedad.save()
+      .then(propiedad => {
+          res.json(propiedad);
+      })
+      .catch(error => {
+          res.status(500).send(error);
+      });
+});
+
+
+app.get('/detalles_propiedad', (req, res) => {
+  Detalles_Propiedad.find({}, (error, detalles_propiedad) => {
+    if (error) {
+      console.log('Error al obtener detalles de las propiedades:', error);
+      res.status(500).json({ error: 'Error al obtener detalles de las propiedades' });
+    } else {
+      res.json(detalles_propiedad);
+    }
+  });
+});
+
+
+app.post('/agregarDetallesPropiedades', (req, res) => {
+  const nuevoDetallesPropiedad= new Detalles_Propiedad({
+      id_propiedad: req.body.id_propiedad,
+      max_huespedes: req.body.max_huespedes,
+      camas: req.body.camas,
+      cocina: req.body.cocina,
+      wifi: req.body.wifi,
+      parqueo: req.body.parqueo,
+  });
+  nuevoDetallesPropiedad.save()
+      .then(detalles_propiedades => {
+          res.json(detalles_propiedades);
+      })
+      .catch(error => {
+          res.status(500).send(error);
+      });
+});
+
+
+
 
 app.get('/favoritos', (req, res) => {
   Favorito.find({})
