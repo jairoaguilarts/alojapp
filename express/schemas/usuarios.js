0e265d6b4233fd553b1f8f68523a763ec8cc98e9
BoroaDev7 <jmborjas@unitec.edu>
2023-06-17 18:26:26 -0600

    Cambios en schemas

--- express/schemas/usuarios.js
+++ express/schemas/usuarios.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const UsuariosSchema = new mongoose.Schema({
-    nombre: String,
-    nombre_usuario: String,
-    contrasenia: String,
-    stripeCustomerId: String,  // ID del cliente de Stripe
-});
+    nombre:{
+        type: String,
+        required: true,
+    },
+    nombre_usuario:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    correo:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    contrasenia:{
+        type: String,
+        required: true,
+    },
+    imagen: Buffer, 
+    stripeCustomerId: String  // ID del cliente de Stripe
+});     
+
+
 
 const usuario = mongoose.model('usuario', UsuariosSchema);
 module.exports = usuario;
