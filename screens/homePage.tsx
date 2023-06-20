7d41de9bb460963021ecb0c252c130728db6435d
jairoaguilarts <jairoaguilar1219@gmail.com>
2023-06-19 20:21:53 -0600

    Iconos en las tabs

--- screens/homePage.tsx
+++ screens/homePage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { Text, View, Image} from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 type RootStackParamList = {
     Inicio: undefined;
@@ -59,7 +59,25 @@ const Tab = createBottomTabNavigator();
 
 const HomePage: React.FC<HomeProps> = ({ navigation }) => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    if (route.name === 'Home') {
+                        return <Image source={require('./icons/home.png')} style={{ width: size, height: size, tintColor: color }} />;
+                    } else if (route.name === 'Favoritos') {
+                        return <Image source={require('./icons/favorites.png')} style={{ width: size, height: size, tintColor: color }} />;
+                    } else if (route.name === 'Rentados') {
+                        return <Image source={require('./icons/rents.png')} style={{ width: size, height: size, tintColor: color }} />;
+                    } else if (route.name === 'Asistencia') {
+                        return <Image source={require('./icons/support.png')} style={{ width: size, height: size, tintColor: color }} />;
+                    } else if (route.name === 'Perfil') {
+                        return <Image source={require('./icons/profile.png')} style={{ width: size, height: size, tintColor: color }} />;
+                    }
+                },
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            })}
+        >
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Favoritos" component={FavoritosScreen} />
             <Tab.Screen name="Rentados" component={RentadosScreen} />
