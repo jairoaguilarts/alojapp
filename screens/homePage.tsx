0acf4a196c6555dab8b2b92b2d78427499dc8b0f
jairoaguilarts <jairoaguilar1219@gmail.com>
2023-06-19 19:48:28 -0600

    Tab en el homepage

--- screens/homePage.tsx
+++ screens/homePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { NavigationProp } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -14,13 +15,57 @@ type HomeProps = {
     navigation: NavigationProp<RootStackParamList, 'HomePage'>;
 };
 
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+
+function FavoritosScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Favoritos!</Text>
+        </View>
+    );
+}
+
+function RentadosScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Rentados!</Text>
+        </View>
+    );
+}
+
+function AsistenciaScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Asistencia!</Text>
+        </View>
+    );
+}
+
+function PerfilScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Perfil!</Text>
+        </View>
+    );
+}
+
 const Tab = createBottomTabNavigator();
 
 const HomePage: React.FC<HomeProps> = ({ navigation }) => {
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Home Page</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Favoritos" component={FavoritosScreen} />
+            <Tab.Screen name="Rentados" component={RentadosScreen} />
+            <Tab.Screen name="Asistencia" component={AsistenciaScreen} />
+            <Tab.Screen name="Perfil" component={PerfilScreen} />
+        </Tab.Navigator>
     );
 };
 
